e63a7fc1c27a7f8eb6ef6a90b0923d21512fadea
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-03-21 21:32:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 YOUTUBE_API_KEY = os.getenv("AIzaSyAOorokSXnBGeDFte5_LoxXWh6MYPIOq7I")  # YouTube API Key
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")  # Telegram Bot का Token
+TELEGRAM_BOT_TOKEN = os.getenv("7605033751:AAFAkK096F2N65qS5pMDGEQfP8FzN5rxtIY")  # Telegram Bot का Token
 CHANNEL_ID = os.getenv("CHANNEL_ID")  # YouTube चैनल ID जिसपर नजर रखनी है
-COMMENT_TEXT = os.getenv("COMMENT_TEXT")  # जो कमेंट करना है
+COMMENT_TEXT = os.getenv("My first comment i am winner")  # जो कमेंट करना है
