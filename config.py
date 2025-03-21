a3a7ecf53b399da8c9756f98cb888610933cf1e8
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-03-21 21:29:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 # .env फाइल से API Keys लोड करना
 load_dotenv()
 
-YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")  # YouTube API Key
+YOUTUBE_API_KEY = os.getenv("AIzaSyAOorokSXnBGeDFte5_LoxXWh6MYPIOq7I")  # YouTube API Key
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")  # Telegram Bot का Token
 CHANNEL_ID = os.getenv("CHANNEL_ID")  # YouTube चैनल ID जिसपर नजर रखनी है
 COMMENT_TEXT = os.getenv("COMMENT_TEXT")  # जो कमेंट करना है
