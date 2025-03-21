fc230e93c98917ddc2cdd30b063e10b5368dd959
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-03-21 22:01:53 +0530

    Update youtube_checker.py

--- youtube_checker.py
+++ youtube_checker.py
@@ -3,25 +3,9 @@ import time
 from config import YOUTUBE_API_KEY, CHANNEL_ID
 
 def get_latest_video():
-    """YouTube चैनल से सबसे नया वीडियो ID निकाले"""
     url = f"https://www.googleapis.com/youtube/v3/search?key={YOUTUBE_API_KEY}&channelId={CHANNEL_ID}&part=snippet,id&order=date&maxResults=1"
     response = requests.get(url).json()
-    
     if "items" in response:
-        video_id = response["items"][0]["id"]["videoId"]
+        video_id = response["items"][0]["id"].get("videoId")
         return video_id
     return None
-
-if __name__ == "__main__":
-    latest_video = None
-
-    while True:
-        video_id = get_latest_video()
-        
-        if video_id and video_id != latest_video:
-            print(f"🔔 नया वीडियो मिला: {video_id}")
-            latest_video = video_id
-            with open("latest_video.txt", "w") as f:
-                f.write(video_id)  # नया वीडियो ID सेव करना
-
-        time.sleep(0.5)  # हर 0.5 सेकंड बाद चेक करना
